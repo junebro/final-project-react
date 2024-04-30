de421eefb4c26802b0238466f29c76646d8a7b68
june <june@naver.com>
2024-04-30 12:25:33 +0900

    css 폴더 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './test/Item';
+import App from './main/main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
