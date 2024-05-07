92107be5c3fb2893180960b9bae86187c47a4118
june <june@naver.com>
2024-05-07 10:43:27 +0900

    주문 페이지, 라우터 수정

---
+++ src/order/order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function order() {
+
+    return(
+        <>
+            
+            <div className='order-container'>
+                <div className='aaa'>aaaaa</div>
+            </div>
+        </>
+    )
+}
+
+export default order;
