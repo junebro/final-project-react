b76e5d1c841b82863c21ad27892c6da73077b15f
jt <1qkrtjsgml1@naver.com>
2024-05-09 09:39:00 +0900

    123

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./main/main";
 import Products from "./products/App";
 import Cart from "./cart/cart";
-
+import Nutri from "./TT/App_nutri";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Route path="/" element={<Main />} />
       <Route path="/Products" element={<Products />} />
       <Route path="/Cart" element={<Cart />} />
+      <Route path="/Nutri/*" element={<Nutri />} />
     </Routes>
   );
 }
