59e117630b0e0c6e296299260a66a9f528cf6a1e
chaeyoung <chylee917@gmail.com>
2024-05-13 17:34:41 +0900

    index.js 경로 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./Router";  // App 컴포넌트, 실제 경로에 맞게 조정 필요
 import { BrowserRouter } from "react-router-dom";
-import { ItemProvider } from './ItemContext';  // ItemProvider import, 실제 경로에 맞게 조정 필요
+import { ItemProvider } from './common/contexts/ItemContext';  // ItemProvider import, 실제 경로에 맞게 조정 필요
 
 const container = document.getElementById("root");
 const root = createRoot(container);
