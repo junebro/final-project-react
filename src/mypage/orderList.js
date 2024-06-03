8cd1a5679d11a4a2a4aef50faa8c227ffe35ac80
seungmi0314 <bbsm314@gmail.com>
2024-06-03 16:32:55 +0900

    주문내역 조회 완료

--- src/mypage/orderList.js
+++ src/mypage/orderList.js
@@ -9,100 +9,88 @@ import { ItemProvider, useItem } from '../common/contexts/MyOListContext';
 import { useAuth } from '../common/contexts/AuthContext'; // 로그인 
 
 function App() {
-    
-    const [items, setItems] = useState([]);
-    const { user } = useAuth();
-    const token = localStorage.getItem('authToken');
+    return (
+        <ItemProvider>
+            <ItemDisplay/>
+        </ItemProvider>
+    );
 
-    // const products = useItem().item; // 전체 제품 목록을 가져옵니다.
-    
+    function ItemDisplay() {
+        const { user } = useAuth(); // useAuth 훅에서 user ID 가져오기
+        const token = localStorage.getItem('authToken');
+        const data = useItem().item;
+        const myOrderData = data?.myOrderList || [];
+
+
+        const groupedOrders = data?.reduce((acc, order) => {
+            acc[order.ordno] = acc[order.ordno] || [];
+            acc[order.ordno].push(order);
+            return acc;
+        }, {})||[];
+
+        console.log(data);
     return (
         <div>
             <Navi />
             <Menu />
+
+            
             <div className="section-p">
-            <div className='order-section'>
-            <nav className="mypage-nav">
-    <div className="nav-profile-img"></div>
-    <p className="nav-nickName">닉네임</p>
-    <hr className="h1" />
-    <ul className="mypage-ul">
-      <li><a href="#">내 정보 수정</a></li>
-      <li><Link to="/mypage/Nutrition" >영양 진단 결과</Link></li>
-      <li><Link to="/mypage/MyCommunity">커뮤니티 활동</Link></li>
-      <li><Link to="/mypage/HealthDiary"  >건강 일기</Link></li>
-      <li><Link to="/mypage/OrderList" className='mypage-menu-active'>주문 내역</Link></li>
-      <li><a href="#">회원 탈퇴</a></li>
-    </ul>
-  </nav>
+                <div className='order-section'>
+                    <nav className="mypage-nav">
+                        <div className="nav-profile-img"></div>
+                        <p className="nav-nickName">닉네임</p>
+                        <hr className="h1" />
+                        <ul className="mypage-ul">
+                        <li><a href="#">내 정보 수정</a></li>
+                        <li><Link to="/mypage/Nutrition" >영양 진단 결과</Link></li>
+                        <li><Link to="/mypage/MyCommunity">커뮤니티 활동</Link></li>
+                        <li><Link to="/mypage/HealthDiary"  >건강 일기</Link></li>
+                        <li><Link to="/mypage/OrderList" className='mypage-menu-active'>주문 내역</Link></li>
+                        <li><a href="#">회원 탈퇴</a></li>
+                        </ul>
+                    </nav>
 
-      <div className="contents">
-      <h1 className="mypage-title">주문 내역</h1>
-            <hr className="title-line" />
+                    <div className="contents">
+                        <h1 className="mypage-title">주문 내역</h1>
+                        <hr className="title-line" />
 
-            <div className="order-box">
-                <div className="delivery-info">
-                    <p className="order-date">2024.05.02</p>
-                    <p className="delivery-status">배송 준비</p>
-                </div>
+                        
 
-                <div className="product-box">
-                    <div className="product-img">
-                        <img src={require("./../images/products/product2.jpg")} alt="프로필 사진" />
+                    <div>
+                        {Object.entries(groupedOrders).map(([ordno, group], index) => (	
+                            <div className="order-box" key={ordno}>
+                                <div className="delivery-info">
+                                    <p className="order-date">{new Date(group[0].orddt).toISOString().slice(0, 10)}</p>
+                                    <p className="delivery-status">배송준비</p>
+                                </div>
+                                    {group.map((item, idx) => (
+                                        <div className="product-box">
+                                            <div className="product-img">
+                                                <img src={require(`./../images/products/${item.proimg}.jpg`)} alt="상품 사진" />
+                                            </div>
+                                            <div className="order-info">
+                                                <p>{item.pronm} | {item.crtqt} 개</p>
+                                                <p>{(item.propr * item.crtqt).toLocaleString('ko-KR')}원</p>
+                                                <p> </p> 
+                                            </div>
+                                        </div>
+                                    ))}
+                                <div className="order-result">
+                                    <span className="payment">총 결제금액 : {group[0].ordpr.toLocaleString('ko-KR')}원</span>
+                                    <button type="button" className="delivery-btn">주문 취소</button>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    <div className="order-info">
-                        <p>리코타치즈 샐러드 | 1 개</p>
-                        <p>20,000원</p>
-                        {/* 주소가 있던자리 빼게되면 lastchild 속성 빼기 */}
-                        <p> </p> 
-                    </div>
-                </div>
 
-                <div className="order-result">
-                    {/* 결제 테이블에서 최종 결제 금액으로 표시하기 */}
-                    <span className="payment">총 결제금액 : 20,000원</span>
-                    <button type="button" className="delivery-btn">주문 취소</button>
+                    
+                    </div>
                 </div>
             </div>
-
-            <div className="order-box">
-                <div className="delivery-info">
-                    <p className="order-date">2024.05.02</p>
-                    <p className="delivery-status">배송 완료</p>
-                </div>
-
-                <div className="product-box">
-                    <div className="product-img">
-                        <img src={require("./../images/products/product1.jpg")} alt="프로필 사진" />
-                    </div>
-                    <div className="order-info">
-                        <p>리코타치즈 샐러드 | 1 개</p>
-                        <p>20,000원</p>
-                        <p>서울특별시 마포구 어디동 300번지 어쩌고 102호 </p>
-                    </div>
-                </div>
-
-                <div className="product-box">
-                    <div className="product-img">
-                        <img src={require("./../images/products/product3.jpg")} alt="프로필 사진" />
-                    </div>
-                    <div className="order-info">
-                        <p>리코타치즈 샐러드 | 1 개</p>
-                        <p>20,000원</p>
-                        <p>서울특별시 마포구 어디동 300번지 어쩌고 102호 </p>
-                    </div>
-                </div>
-
-                <div className="order-result">
-                    <span className="payment">총 결제금액 : 20,000원</span>
-                    <button type="button" className="delivery-btn">반품 신청</button>
-                </div>
-            </div>
+            <Footer />
         </div>
-    </div>
-    </div>
-        <Footer />
-    </div>
-    );    
+        );    
+    }
 }
 export default App;
