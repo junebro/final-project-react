cc0c561ceff0b867849f995b36df71b3fb109c56
jt <1qkrtjsgml1@naver.com>
2024-06-04 11:47:57 +0900

    ㅁㄴㅇㄴㅁㅇ

--- src/mypage/nutritionNutrient.js
+++ src/mypage/nutritionNutrient.js
@@ -1,38 +1,91 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./../TT/css/Nutrient_css.css";
 import Chart from "./../TT/components/chart";
 import { Link } from "react-router-dom";
 import { useAuth } from "./../common/contexts/AuthContext";
+import { NutriContext, NutriProvider } from "./../TT/Nutri_Context";
 
-function App() {
-  const [responseData, setResponseData] = useState(null); // 초기 상태를 null로 설정
-  const [userName, setUserName] = useState('');
-  const [nutriGenderState, setNutriGenderState] = useState('');
-  const [nutriHeight, setNutriHeight] = useState('');
-  const [nutriWeight, setNutriWeight] = useState('');
+function Display() {
+  const {
+    nutriGenderState,
+    setNutriGenderState,
+    nutriAge,
+    setNutriAge,
+    nutriHeight,
+    setNutriHeight,
+    nutriWeight,
+    setNutriWeight,
+    selectedDisease,
+    selectedAllergies,
+    exerciseRate,
+    soup,
+    alcoholRate,
+    setResponseData,
+    responseData,
+    userName,
+    setUserName,
+    setDrag1Items,
+    setDrag2Items,
+    setDrag3Items,
+    setDrag4Items,
+    setDrag5Items,
+    setDrag6Items,
+  } = useContext(NutriContext);
+
   let disease = "";
   useEffect(() => {
-    const token = localStorage.getItem('authToken');
+    if (user != null) {
+      // 유저 이름 받아오기
+      fetch("/nutri/sendUserId01", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: user,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(
+              "Network response was not ok " + response.statusText
+            );
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setUserName(data.memberNick); // 응답 데이터를 상태에 저장
+          console.log("닉네임 : " + data.memberNick);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }, []);
+
+  console.log(userName);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
     // 서버에서 상품 정보 가져오기
     fetch(`http://localhost:8989/nutrition/nutrientselect/9`, {
-      method: 'GET', // HTTP 메소드 지정
+      method: "GET", // HTTP 메소드 지정
       headers: {
-        'Content-Type': 'application/json', // 콘텐츠 타입 지정
-        'Authorization': `Bearer ${token}` // JWT 토큰을 Bearer 토큰으로 포함
-      }
+        "Content-Type": "application/json", // 콘텐츠 타입 지정
+        Authorization: `Bearer ${token}`, // JWT 토큰을 Bearer 토큰으로 포함
+      },
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      setResponseData(data); // 필터링된 제품 목록을 상태에 설정
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setResponseData(data); // 필터링된 제품 목록을 상태에 설정
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
   }, []); // 의존성 배열에 selectedMenu, products를 포함시켜 상태 변화를 감지합니다.
 
   const { user } = useAuth(); // useAuth 훅에서 user ID 가져오기
@@ -54,14 +107,20 @@ function App() {
   };
 
   // 탄단지 비율 계산
-  const calc = responseData.carbohydrateRate + responseData.proteinRate + responseData.fatRate;
-  const carbohydrateRate = Math.floor((responseData.carbohydrateRate / calc) * 100);
+  const calc =
+    responseData.carbohydrateRate +
+    responseData.proteinRate +
+    responseData.fatRate;
+  const carbohydrateRate = Math.floor(
+    (responseData.carbohydrateRate / calc) * 100
+  );
   const proteinRate = Math.floor((responseData.proteinRate / calc) * 100);
   const fatRate = Math.floor((responseData.fatRate / calc) * 100);
 
   // 칼로리 계산 구역
   const calcorie_calc = () => {
-    const result = (responseData.calories / (responseData.requiredCalories * 2)) * 100;
+    const result =
+      (responseData.calories / (responseData.requiredCalories * 2)) * 100;
     return result;
   };
 
@@ -72,10 +131,12 @@ function App() {
     const rcl = responseData.requiredCalories;
 
     if (cl > rcl) {
-      message = "칼로리 과다 섭취는 건강에 해를 끼칠 수 있습니다. 지나친 섭취는 체중 증가와 다양한 질병의 위험을 높일 수 있으니 주의가 필요합니다. 식사를 할 때 음식의 양과 종류를 신중하게 선택하여 건강한 식습관을 유지하는 것이 중요합니다. 균형 잡힌 식단과 적절한 운동을 통해 건강을 유지하세요.!";
+      message =
+        "칼로리 과다 섭취는 건강에 해를 끼칠 수 있습니다. 지나친 섭취는 체중 증가와 다양한 질병의 위험을 높일 수 있으니 주의가 필요합니다. 식사를 할 때 음식의 양과 종류를 신중하게 선택하여 건강한 식습관을 유지하는 것이 중요합니다. 균형 잡힌 식단과 적절한 운동을 통해 건강을 유지하세요.!";
       html = <div style={{ fontSize: "24px", color: "red" }}>&nbsp;과다</div>;
     } else if (cl < rcl) {
-      message = "칼로리를 적정하게 유지하고 있는 당신을 칭찬합니다! 건강한 식습관을 유지하면서 몸과 마음을 케어하는 당신의 노력은 대단합니다. 적절한 칼로리 섭취는 건강한 생활의 중요한 요소이며, 당신의 노력은 당신의 건강을 지키는 데 큰 도움이 됩니다. 계속해서 이렇게 건강한 습관을 유지해 나가면서 더욱 좋은 결과를 얻을 수 있을 것입니다. 축하합니다!";
+      message =
+        "칼로리를 적정하게 유지하고 있는 당신을 칭찬합니다! 건강한 식습관을 유지하면서 몸과 마음을 케어하는 당신의 노력은 대단합니다. 적절한 칼로리 섭취는 건강한 생활의 중요한 요소이며, 당신의 노력은 당신의 건강을 지키는 데 큰 도움이 됩니다. 계속해서 이렇게 건강한 습관을 유지해 나가면서 더욱 좋은 결과를 얻을 수 있을 것입니다. 축하합니다!";
       html = <div style={{ fontSize: "24px", color: "Green" }}>&nbsp;적정</div>;
     }
 
@@ -99,14 +160,19 @@ function App() {
     let sl = responseData.sodium;
 
     if (sl > 2.5) {
-      message = "나트륨 과다 섭취는 고혈압, 심장 질환, 뇌졸중과 같은 심각한 건강 문제를 유발할 수 있습니다. 과다한 나트륨 섭취는 혈압을 높일 뿐만 아니라 신장에 부담을 주고 체액이 늘어나는 문제를 일으키며, 이는 심장 및 혈관 건강을 해칠 수 있습니다. 또한, 과다한 나트륨은 식품 중독 및 소화 장애를 유발할 수 있으며, 뼈 건강과 치아 건강에도 부정적인 영향을 줄 수 있습니다. 따라서 건강한 식습관을 유지하고 나트륨 섭취를 제한하여 건강을 유지하는 것이 중요합니다.";
+      message =
+        "나트륨 과다 섭취는 고혈압, 심장 질환, 뇌졸중과 같은 심각한 건강 문제를 유발할 수 있습니다. 과다한 나트륨 섭취는 혈압을 높일 뿐만 아니라 신장에 부담을 주고 체액이 늘어나는 문제를 일으키며, 이는 심장 및 혈관 건강을 해칠 수 있습니다. 또한, 과다한 나트륨은 식품 중독 및 소화 장애를 유발할 수 있으며, 뼈 건강과 치아 건강에도 부정적인 영향을 줄 수 있습니다. 따라서 건강한 식습관을 유지하고 나트륨 섭취를 제한하여 건강을 유지하는 것이 중요합니다.";
       html = <div style={{ fontSize: "24px", color: "red" }}>&nbsp;과다</div>;
     } else if (sl > 1.5 && sl < 2.5) {
-      message = "나트륨을 적정량으로 섭취하여 건강에 주의를 기울이는 당신은 정말 멋지세요! 균형 잡힌 식습관을 유지하고, 식이섬유와 식이지방 등의 영양소도 적절하게 섭취하는 것은 건강한 삶을 살아가는 데 중요한 요소입니다. 적정량의 나트륨 섭취는 혈압을 유지하고 심혈관 건강을 지키는 데 도움을 줄 뿐만 아니라 신체의 수분 균형을 유지하여 건강한 생활을 뒷받침합니다. 지속적으로 이러한 건강한 습관을 유지하고 자신의 건강에 관심을 가지는 당신의 노력과 의지를 칭찬합니다! 계속해서 건강한 선택을 이어가며 더 나은 삶을 살아가길 응원합니다.";
+      message =
+        "나트륨을 적정량으로 섭취하여 건강에 주의를 기울이는 당신은 정말 멋지세요! 균형 잡힌 식습관을 유지하고, 식이섬유와 식이지방 등의 영양소도 적절하게 섭취하는 것은 건강한 삶을 살아가는 데 중요한 요소입니다. 적정량의 나트륨 섭취는 혈압을 유지하고 심혈관 건강을 지키는 데 도움을 줄 뿐만 아니라 신체의 수분 균형을 유지하여 건강한 생활을 뒷받침합니다. 지속적으로 이러한 건강한 습관을 유지하고 자신의 건강에 관심을 가지는 당신의 노력과 의지를 칭찬합니다! 계속해서 건강한 선택을 이어가며 더 나은 삶을 살아가길 응원합니다.";
       html = <div style={{ fontSize: "24px", color: "Green" }}>&nbsp;적정</div>;
     } else if (sl < 1.5) {
-      message = "나트륨 부족은 신체의 전해질 균형을 무너뜨려 어지럼증, 피로, 근육 경련, 저혈압 등의 증상을 초래할 수 있습니다. 심할 경우, 혼란, 발작 등의 심각한 문제를 유발할 수 있습니다. 균형 잡힌 식단으로 적절한 나트륨을 섭취하여 건강을 유지하세요.";
-      html = <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>;
+      message =
+        "나트륨 부족은 신체의 전해질 균형을 무너뜨려 어지럼증, 피로, 근육 경련, 저혈압 등의 증상을 초래할 수 있습니다. 심할 경우, 혼란, 발작 등의 심각한 문제를 유발할 수 있습니다. 균형 잡힌 식단으로 적절한 나트륨을 섭취하여 건강을 유지하세요.";
+      html = (
+        <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>
+      );
     }
 
     return { message, html };
@@ -129,21 +195,28 @@ function App() {
     let dl = responseData.dietaryFiber;
 
     if (dl >= 55) {
-      message = "식이섬유를 과다하게 섭취하면 소화 문제를 유발할 수 있습니다. 소화기 건강을 유지하기 위해 매일 권장 섭취량을 초과하지 않도록 주의하세요. 과다한 섬유는 변비, 복통, 가스 등을 유발할 수 있습니다. 균형 잡힌 식단을 유지하고 적당량의 섬유를 섭취하세요.";
+      message =
+        "식이섬유를 과다하게 섭취하면 소화 문제를 유발할 수 있습니다. 소화기 건강을 유지하기 위해 매일 권장 섭취량을 초과하지 않도록 주의하세요. 과다한 섬유는 변비, 복통, 가스 등을 유발할 수 있습니다. 균형 잡힌 식단을 유지하고 적당량의 섬유를 섭취하세요.";
       html = <div style={{ fontSize: "24px", color: "red" }}>&nbsp;과다</div>;
     } else if (dl > 20 && dl < 55) {
-      message = "식이섬유를 적정량 섭취하셨군요! 멋진 선택입니다. 식이섬유는 소화에 도움을 주고, 혈당 조절과 콜레스테롤 감소에 도움이 됩니다. 건강한 소화와 올바른 영양 공급에 이바지하고 있어요. 계속해서 다양한 채소, 과일, 견과류를 섭취하여 건강을 유지해보세요!";
+      message =
+        "식이섬유를 적정량 섭취하셨군요! 멋진 선택입니다. 식이섬유는 소화에 도움을 주고, 혈당 조절과 콜레스테롤 감소에 도움이 됩니다. 건강한 소화와 올바른 영양 공급에 이바지하고 있어요. 계속해서 다양한 채소, 과일, 견과류를 섭취하여 건강을 유지해보세요!";
       html = <div style={{ fontSize: "24px", color: "Green" }}>&nbsp;적정</div>;
     } else if (dl < 20) {
-      message = "식이섬유가 부족하면 소화 문제가 발생할 수 있으며 변비, 소화불량, 체중증가 등의 위험이 증가할 수 있습니다. 부족한 식이섬유는 심장 질환, 당뇨병, 대장암 등의 만성질환 발병 위험도 증가시킬 수 있습니다. 채소, 과일, 곡물 등 다양한 식품을 섭취하여 하루에 적어도 25g 이상의 식이섬유를 섭취하도록 노력하세요. 건강한 식단으로 소화 건강을 유지하고 심장과 전반적인 건강을 지키세요.";
-      html = <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>;
+      message =
+        "식이섬유가 부족하면 소화 문제가 발생할 수 있으며 변비, 소화불량, 체중증가 등의 위험이 증가할 수 있습니다. 부족한 식이섬유는 심장 질환, 당뇨병, 대장암 등의 만성질환 발병 위험도 증가시킬 수 있습니다. 채소, 과일, 곡물 등 다양한 식품을 섭취하여 하루에 적어도 25g 이상의 식이섬유를 섭취하도록 노력하세요. 건강한 식단으로 소화 건강을 유지하고 심장과 전반적인 건강을 지키세요.";
+      html = (
+        <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>
+      );
     }
 
     return { message, html };
   };
 
   // 콜레스테롤 비율 계산
-  let cholesterol_indicator = Math.floor((responseData.cholesterol / 0.6) * 100);
+  let cholesterol_indicator = Math.floor(
+    (responseData.cholesterol / 0.6) * 100
+  );
   if (cholesterol_indicator > 100) {
     cholesterol_indicator = 95;
   }
@@ -154,10 +227,12 @@ function App() {
     let cl = responseData.cholesterol;
 
     if (cl > 0.3) {
-      message = "콜레스테롤을 과다 섭취하면 심혈관 질환과 관련된 위험이 증가할 수 있습니다. 지방 함량이 높은 식품을 줄이고, 트랜스 지방과 포화 지방을 피하며, 채식 식단을 채택해보세요. 건강한 지방을 위해 엑스트라 버진 올리브 오일이나 아보카도 같은 단일 불포화 지방을 선택하고, 콜레스테롤을 낮추는 음식인 옥수수 식이섬유와 오메가-3 지방산이 풍부한 생선을 섭취하세요. 건강한 식습관과 꾸준한 운동은 콜레스테롤 수치를 개선할 수 있습니다. 이러한 조치를 통해 심혈관 건강을 챙기세요!";
+      message =
+        "콜레스테롤을 과다 섭취하면 심혈관 질환과 관련된 위험이 증가할 수 있습니다. 지방 함량이 높은 식품을 줄이고, 트랜스 지방과 포화 지방을 피하며, 채식 식단을 채택해보세요. 건강한 지방을 위해 엑스트라 버진 올리브 오일이나 아보카도 같은 단일 불포화 지방을 선택하고, 콜레스테롤을 낮추는 음식인 옥수수 식이섬유와 오메가-3 지방산이 풍부한 생선을 섭취하세요. 건강한 식습관과 꾸준한 운동은 콜레스테롤 수치를 개선할 수 있습니다. 이러한 조치를 통해 심혈관 건강을 챙기세요!";
       html = <div style={{ fontSize: "24px", color: "red" }}>&nbsp;과다</div>;
     } else if (cl < 0.3) {
-      message = "콜레스테롤을 적정량으로 섭취하셨군요! 건강한 심혈관을 유지하는 데 중요한 역할을 합니다. 식이섬유와 단일 불포화 지방을 공급하는 채소, 과일, 견과류, 양식 생선 등 다양한 식품을 섭취하시면서 콜레스테롤을 조절하세요. 이는 심혈관 질환 예방과 혈중 콜레스테롤 수치를 적정하게 유지하는 데 도움이 됩니다. 계속해서 건강한 식습관을 유지하고, 꾸준한 운동을 통해 심혈관 건강을 지켜나가세요!";
+      message =
+        "콜레스테롤을 적정량으로 섭취하셨군요! 건강한 심혈관을 유지하는 데 중요한 역할을 합니다. 식이섬유와 단일 불포화 지방을 공급하는 채소, 과일, 견과류, 양식 생선 등 다양한 식품을 섭취하시면서 콜레스테롤을 조절하세요. 이는 심혈관 질환 예방과 혈중 콜레스테롤 수치를 적정하게 유지하는 데 도움이 됩니다. 계속해서 건강한 식습관을 유지하고, 꾸준한 운동을 통해 심혈관 건강을 지켜나가세요!";
       html = <div style={{ fontSize: "24px", color: "Green" }}>&nbsp;적정</div>;
     }
 
@@ -169,53 +244,34 @@ function App() {
   if (Protein_indicator > 100) {
     Protein_indicator = 95;
   }
-  const Protein_info = Math.floor((responseData.protein / responseData.requiredProtein) * 100);
+  const Protein_info = Math.floor(
+    (responseData.protein / responseData.requiredProtein) * 100
+  );
 
   // 단백질 과다 섭취 메세지
   const Protein_message = () => {
     let message = "";
     let html = "";
     if (responseData.protein > responseData.requiredProtein) {
-      message = "과다한 단백질 섭취는 신장에 부담을 주고, 신장 질환을 유발할 수 있으며, 체내 칼슘 손실을 증가시켜 골다공증의 위험을 높일 수 있습니다. 또한, 소화 문제와 관련된 부작용을 초래할 수 있습니다.";
+      message =
+        "과다한 단백질 섭취는 신장에 부담을 주고, 신장 질환을 유발할 수 있으며, 체내 칼슘 손실을 증가시켜 골다공증의 위험을 높일 수 있습니다. 또한, 소화 문제와 관련된 부작용을 초래할 수 있습니다.";
       html = <div style={{ fontSize: "24px", color: "red" }}>&nbsp;과다</div>;
     } else if (responseData.protein < responseData.minProtein) {
-      message = "단백질 부족은 근육 손실, 면역력 저하, 상처 치유 지연, 피로감 증가 등을 초래할 수 있습니다. 균형 잡힌 식단으로 충분한 단백질 섭취가 필요합니다.";
+      message =
+        "단백질 부족은 근육 손실, 면역력 저하, 상처 치유 지연, 피로감 증가 등을 초래할 수 있습니다. 균형 잡힌 식단으로 충분한 단백질 섭취가 필요합니다.";
       html = <div style={{ fontSize: "24px", color: "Green" }}>&nbsp;적정</div>;
     } else {
-      message = "적정 단백질 섭취로 건강을 지키는 당신, 정말 멋져요! 계속 유지하세요!";
-      html = <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>;
+      message =
+        "적정 단백질 섭취로 건강을 지키는 당신, 정말 멋져요! 계속 유지하세요!";
+      html = (
+        <div style={{ fontSize: "24px", color: "#FFC939" }}>&nbsp;부족</div>
+      );
     }
     return { message, html };
   };
 
   return (
     <div className="center_container_1">
-      <div className="customerName">
-        <div>{userName}</div>
-        <div>{getCurrentDate()}</div>
-      </div>
-      <div className="customerInfo">
-        <div>
-          <p className="pt_1">고객 정보</p>
-          <p>
-            {userName} / {gender}
-          </p>
-        </div>
-        <div>
-          <p className="pt_1">키/몸무게</p>
-          <p>
-            {nutriHeight}cm / {nutriWeight}kg
-          </p>
-        </div>
-        <div>
-          <p className="pt_1">질병</p>
-          <p>{disease}</p>
-        </div>
-        <div>
-          <p className="pt_1">고객 정보</p>
-          <p>해당없는색</p>
-        </div>
-      </div>
       <br />
       <br />
       <br />
@@ -258,12 +314,20 @@ function App() {
                 <div className="bar_info">
                   <div style={{ height: "100%", width: "15%" }}></div>
                   <div
-                    style={{ height: "100%", width: "20%", textAlign: "right" }}
+                    style={{
+                      height: "100%",
+                      width: "20%",
+                      textAlign: "right",
+                    }}
                   >
                     {Math.floor(responseData.requiredCalories)}kcal
                   </div>
                   <div
-                    style={{ height: "100%", width: "65%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "65%",
+                      textAlign: "left",
+                    }}
                   ></div>
                 </div>
                 <div className="bar_container">
@@ -296,12 +360,20 @@ function App() {
                 <div className="bar_info">
                   <div style={{ height: "100%", width: "15%" }}></div>
                   <div
-                    style={{ height: "100%", width: "20%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "20%",
+                      textAlign: "left",
+                    }}
                   >
                     1500mg
                   </div>
                   <div
-                    style={{ height: "100%", width: "65%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "65%",
+                      textAlign: "left",
+                    }}
                   >
                     2500mg
                   </div>
@@ -335,12 +407,20 @@ function App() {
                 <div className="bar_info">
                   <div style={{ height: "100%", width: "15%" }}></div>
                   <div
-                    style={{ height: "100%", width: "20%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "20%",
+                      textAlign: "left",
+                    }}
                   >
                     20g
                   </div>
                   <div
-                    style={{ height: "100%", width: "65%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "65%",
+                      textAlign: "left",
+                    }}
                   >
                     50g
                   </div>
@@ -370,12 +450,20 @@ function App() {
                 <div className="bar_info">
                   <div style={{ height: "100%", width: "15%" }}></div>
                   <div
-                    style={{ height: "100%", width: "20%", textAlign: "right" }}
+                    style={{
+                      height: "100%",
+                      width: "20%",
+                      textAlign: "right",
+                    }}
                   >
                     300mg
                   </div>
                   <div
-                    style={{ height: "100%", width: "65%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "65%",
+                      textAlign: "left",
+                    }}
                   ></div>
                 </div>
                 <div className="bar_container">
@@ -405,12 +493,20 @@ function App() {
                 <div className="bar_info">
                   <div style={{ height: "100%", width: "15%" }}></div>
                   <div
-                    style={{ height: "100%", width: "20%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "20%",
+                      textAlign: "left",
+                    }}
                   >
                     {responseData.minProtein}g
                   </div>
                   <div
-                    style={{ height: "100%", width: "65%", textAlign: "left" }}
+                    style={{
+                      height: "100%",
+                      width: "65%",
+                      textAlign: "left",
+                    }}
                   >
                     {responseData.requiredProtein}g
                   </div>
@@ -458,6 +554,14 @@ function App() {
         </div>
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <NutriProvider>
+      <Display />
+    </NutriProvider>
   );
 }
 
